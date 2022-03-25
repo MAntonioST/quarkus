71ad7b3e22fc2b719e15cb3005299fc2348e3492
MAntonioST <marco.teixeira@fatec.sp.gov.br>
2022-03-25 12:23:12 -0300

    criado usuario

---
+++ src/main/java/io/github/marcot/quarkussocial/rest/UserResource.java
@@ -0,0 +1,64 @@
+package io.github.marcot.quarkussocial.rest;
+
+import io.github.marcot.quarkussocial.domain.entity.User;
+import io.github.marcot.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @POST
+    @Transactional
+    public Response createUser( CreateUserRequest userRequest ){
+
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAllUsers(){
+        PanacheQuery<User> query = User.findAll();
+        return Response.ok(query.list()).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser( @PathParam("id") Long id){
+        User user = User.findById(id);
+
+        if(user != null){
+            user.delete();
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser( @PathParam("id") Long id, CreateUserRequest userData ){
+        User user = User.findById(id);
+
+        if(user != null){
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+}
